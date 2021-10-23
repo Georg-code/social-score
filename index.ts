17e866ca98ed4f914caf6bf5fb0f72cf36099905
Gerog-code <jornig@outlook.com>
2021-10-24 01:14:11 +0200

    .

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import { Client, Intents } from "discord.js";
 import dotenv from "dotenv";
 import * as score from "./scoreSettings.json";
-import { JsonDB } from 'node-json-db';
-import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
+import { JsonDB } from "node-json-db";
+import { Config } from "node-json-db/dist/lib/JsonDBConfig";
 
-const db = new JsonDB(new Config("citizen", true, false, '/'));
-
-
+const db = new JsonDB(new Config("citizen", true, false, "/"));
 
 dotenv.config();
 const client = new Client({
@@ -22,90 +20,120 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  if (message.author.id === client.user.id) return;
-  const messagecnt = message.content.toLocaleLowerCase();
-  let points = score["v+1"];
+  if (message.channel.id == "901214528522575875") {
+    if (message.author.id === client.user.id) return;
+    const messagecnt = message.content.toLocaleLowerCase();
+    let points = score["v+1"];
 
+    if (message.content.startsWith("!score")) {
+      try {
+        points += score["v-2"];
+        message.reply(
+          `Du hast ${parseInt(
+            db.getData(`/users/${message.author.id}/points`)
+          )} Punkte`
+        );
+      } catch {
+        message.reply("retry..");
+      }
+    }
 
-  if (message.content.startsWith("!score")) {
+    for (const i of score.negative1) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        message.react("🚫");
+        points += score["v-1"];
+      }
+    }
+    for (const i of score.negative2) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        message.react("🚫");
+        points += score["v-2"];
+      }
+    }
+    for (const i of score.negative3) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        message.react("🚫");
+        points += score["v-3"];
+      }
+    }
 
+    for (const i of score.postive) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        message.react("✅");
+        points += score["v+2"];
+      }
+    }
+
+    for (const i of score.xi_name) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        for (const ib of score.xi_positive) {
+          if (messagecnt.includes(ib.toLocaleLowerCase())) {
+            points += score["v+3"];
+            message.react("✅");
+          }
+        }
+        for (const ic of score.xi_negative) {
+          if (messagecnt.includes(ic.toLocaleLowerCase())) {
+            points += score["v-3"];
+            message.react("🚫");
+          }
+        }
+      }
+    }
+
+    for (const i of score.negation) {
+      if (messagecnt.includes(i.toLocaleLowerCase())) {
+        points = score["v-2"];
+        message.react("❓");
+      }
+    }
+    console.log(`Points: ${points}`);
+
+    let dbpoints = 0;
     try {
-      points += score["v-2"];
-      message.reply(`Du hast ${parseInt(db.getData(`/users/${message.author.id}/points`))} Punkte`);
-      
+      dbpoints = parseInt(db.getData(`/users/${message.author.id}/points`));
     } catch {
-      message.reply("retry..")
+      dbpoints = 997;
     }
 
-  }
+    db.push(
+      `/users/${message.author.id}/`,
+      { points: dbpoints + points },
+      false
+    );
 
-
-
-  for (const i of score.negative1) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      message.react("🚫");
-      points += score["v-1"];
-      
-    }
+    const setMute = async (status : boolean) => {
+      try {
+    await  message.member.voice.setMute(status);
+      } catch {
+        console.log("Not in voice")
   }
-  for (const i of score.negative2) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      message.react("🚫");
-      points += score["v-2"];
-    }
-  }
-  for (const i of score.negative3) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      message.react("🚫");
-      points += score["v-3"];
-    }
-  }
-
-   for (const i of score.postive) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      message.react("✅");
-      points += score["v+2"];
-    }
-  }
-
-  for (const i of score.xi_name) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      for (const ib of score.xi_positive) {
-        if (messagecnt.includes(ib.toLocaleLowerCase())) {
-          points += score["v+3"];
-          message.react("✅");
-        }
-      }
-      for (const ic of score.xi_negative) {
-        if (messagecnt.includes(ic.toLocaleLowerCase())) {
-          points += score["v-3"];
-          message.react("🚫");
-        }
-      }
-    }
-  }
-
-  for (const i of score.negation) {
-    if (messagecnt.includes(i.toLocaleLowerCase())) {
-      points = score["v-2"];
-      message.react("❓");
-    }
-  }
-  console.log(`Points: ${points}`);
-
-  let dbpoints = 0;
-  try {
-     dbpoints = parseInt(db.getData(`/users/${message.author.id}/points`));
-  } catch {
-   dbpoints = 997 
 }
 
-  db.push(`/users/${message.author.id}/`, { points: dbpoints + points }, false);
 
-  if (dbpoints + points <= 0) {
-    message.member.voice.setMute(true);
-  } else {
-     message.member.voice.setMute(false);
+    if (dbpoints + points <= 0) {
+      setMute(true);
+    } else {
+      setMute(false);
+    }
   }
 });
+
+
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if(oldState.channelId === newState.channelId) {
+        console.log('a user has not moved!')
+    }
+    if(oldState.channelId != null && newState.channelId != null && newState.channelId != oldState.channelId) {
+        console.log('a user switched channels')
+    }
+    if(oldState.channelId === null) {
+        console.log('a user joined!')
+    }
+    if (newState.channelId === null) {
+        console.log('a user left!')
+    }
+});
+
 client.login(process.env.TOKEN);
